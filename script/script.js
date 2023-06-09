a6c9ab7306fcdb5a739d37b608be8e6553013fec
TALEX1995 <tanase.alex@libero.it>
2023-06-09 16:22:06 +0200

    Get elements from DOM

--- script/script.js
+++ script/script.js
@@ -1,1 +1,16 @@
 console.log('JS OK')
+
+// Get elements from the DOM
+
+const timer = document.getElementById('timer');
+const startGame = document.getElementById('start-game');
+const randomNumberList = document.getElementById('random-number-list');
+const numberInput = document.getElementById('number-input');
+const result = document.getElementById('result');
+
+
+// Function click on the play button
+
+startGame.addEventListener('click', () => {
+    
+})
