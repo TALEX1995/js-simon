79d161f235b7474b21c5b95a7436355ee6d357e8
TALEX1995 <tanase.alex@libero.it>
2023-06-09 16:38:25 +0200

    Added event listener and timing function

--- script/script.js
+++ script/script.js
@@ -9,8 +9,25 @@ const numberInput = document.getElementById('number-input');
 const result = document.getElementById('result');
 
 
+
+// Variable for the Timing function
+let intervalCountdown;
+
+
+
 // Function click on the play button
 
 startGame.addEventListener('click', () => {
+    // Countdown timer
+    let timerCountdown = 30
+    // Timing function for the timer countdown
+    intervalCountdown = setInterval (() => {
+        timerCountdown--,
+        timer.innerText = timerCountdown;
+        if (timerCountdown === 0) {
+            clearInterval(intervalCountdown)
+        }
+    }, 1000);
+    
     
 })
